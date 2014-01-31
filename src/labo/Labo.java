b304e217693937f45a91d7e75111e763f812239a
Chris <Chris@172.29.107.107>
2014-01-30 21:50:55 -0500

    test

--- src/labo/Labo.java
+++ src/labo/Labo.java
@@ -16,7 +16,7 @@ public class Labo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bonjour") ;
+        System.out.println("Fuck you Rhoubins") ;
     }
     
 }
